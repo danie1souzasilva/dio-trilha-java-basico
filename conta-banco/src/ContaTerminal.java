31773e81f3204140b028df2e5d356a096ead65ae
danie1souzasilva <119264364+danie1souzasilva@users.noreply.github.com>
2023-05-31 19:30:11 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int saldo = 1999;
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Olá, digite seu nome: " );
+        String nome = entrada.next();
+        System.out.print("Agora digite sua conta: ");
+        int conta = entrada.nextInt();
+        System.out.print("Digite sua Agencia: ");
+        String agencia = entrada.next();
+        System.out.print("Seu saldo é: " + saldo);
+
+
+
+
+        entrada.close();
+
+
     }
 }
